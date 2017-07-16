d6517f8bd1e00158ff90ace22f9dbcc05e922aea
Stefan Johnson <sj@scrimpycat.io>
2017-07-16 23:53:54 +1000

    Flatten ingredient tree

---
+++ lib/yum/ingredient.ex
@@ -0,0 +1,35 @@
+defmodule Yum.Ingredient do
+    defstruct [
+        ref: nil,
+        translation: %{},
+        exclude_diet: [],
+        exclude_allergen: [],
+        nutrition: %{},
+        group: nil
+    ]
+
+    def new(data), do: Enum.reduce(data, [], &new(&1, &2, %Yum.Ingredient{}))
+
+    defp new({ key, value = %{ __info__: info } }, ingredients, group) do
+        ingredient = %Yum.Ingredient{
+            ref: "#{group.ref}/#{key}",
+            translation: info.translation,
+            group: group.ref
+        }
+        |> new_exclude_diet(info, group)
+        |> new_exclude_allergen(value, group)
+        |> new_nutrition(value)
+
+        [ingredient|Enum.reduce(value, ingredients, &new(&1, &2, ingredient))]
+    end
+    defp new(_, ingredients, _), do: ingredients
+
+    defp new_exclude_diet(ingredient, %{ "exclude-diet": diets }, parent), do: %{ ingredient | exclude_diet: Enum.uniq(diets ++ parent.exclude_diet) }
+    defp new_exclude_diet(ingredient, _, parent), do: %{ ingredient | exclude_diet: parent.exclude_diet }
+
+    defp new_exclude_allergen(ingredient, %{ "exclude-allergen": allergens }, parent), do: %{ ingredient | exclude_allergen: Enum.uniq(allergens ++ parent.exclude_allergen) }
+    defp new_exclude_allergen(ingredient, _, parent), do: %{ ingredient | exclude_allergen: parent.exclude_allergen }
+
+    defp new_nutrition(ingredient, %{ "nutrition": nutrition }), do: %{ ingredient | nutrition: nutrition }
+    defp new_nutrition(ingredient, _), do: ingredient
+end
