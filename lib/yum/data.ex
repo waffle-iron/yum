9b1e3baaa4550c436b3454b88042617b3c140a55
Stefan Johnson <sj@scrimpycat.io>
2017-07-22 01:26:48 +1000

    Document data import functions

--- lib/yum/data.ex
+++ lib/yum/data.ex
@@ -1,4 +1,7 @@
 defmodule Yum.Data do
+    @moduledoc """
+      Import food data.
+    """
     @type translation :: %{ optional(String.t) => translation | String.t }
     @type translation_tree :: %{ optional(String.t) => translation }
     @type diet_list :: [String.t]
@@ -11,15 +14,27 @@ defmodule Yum.Data do
 
     @path "data/Food-Data"
 
+    @doc """
+      Load the diet names and translations.
+    """
     @spec diets(String.t) :: translation_tree
     def diets(data \\ @path), do: load(Path.join(data, "translations/diet-names.toml"))
 
+    @doc """
+      Load the allergen names and translations.
+    """
     @spec allergens(String.t) :: translation_tree
     def allergens(data \\ @path), do: load(Path.join(data, "translations/allergen-names.toml"))
 
+    @doc """
+      Load the ingredient data.
+    """
     @spec ingredients(String.t) :: ingredient_tree
     def ingredients(group \\ "", data \\ @path), do: load_tree(Path.join([data, "ingredients", group]))
 
+    @doc """
+      Load the cuisine data.
+    """
     @spec cuisines(String.t) :: cuisine_tree
     def cuisines(group \\ "", data \\ @path), do: load_tree(Path.join([data, "cuisines", group]))
 
